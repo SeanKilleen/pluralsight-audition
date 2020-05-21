776033a5a83db3c5c9fea0709147e068fe2696f6
Sean Killeen <SeanKilleen@gmail.com>
2020-05-21 13:52:50 -0400

    remove secre addition

--- environment-setup.tf
+++ environment-setup.tf
@@ -84,10 +84,3 @@ data "azurerm_key_vault" "example" {
 output "vault_uri" {
   value = data.azurerm_key_vault.example.vault_uri
 }
-
-resource "azurerm_key_vault_secret" "example" {
-  name         = "secret-sauce"
-  value        = "szechuan"
-  key_vault_id = azurerm_key_vault.example.id
-
-}
