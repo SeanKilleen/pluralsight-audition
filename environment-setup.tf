e7496de19d0d0a60c08164f1948082840d6ae00c
Sean Killeen <SeanKilleen@gmail.com>
2020-05-21 08:31:32 -0400

    initial shot

---
+++ environment-setup.tf
@@ -0,0 +1,102 @@
+provider "azurerm" {
+  version = "=2.0.0"
+  features {} # This is required to prevent some issues around nullability
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "example" {
+  name     = "pluralsight-demo"
+  location = "eastus2"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "pluralsightdemo"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "function-app-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind                = "FunctionApp"
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_function_app" "example" {
+  name                       = "my-function-app"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  app_service_plan_id        = azurerm_app_service_plan.example.id
+  storage_connection_string  = azurerm_storage_account.example.primary_connection_string
+}
+
+resource "azurerm_key_vault" "example" {
+  name                        = "my-key-vault"
+  location                    = azurerm_resource_group.example.location
+  resource_group_name         = azurerm_resource_group.example.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_enabled         = true
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+#   access_policy {
+#     tenant_id = data.azurerm_client_config.current.tenant_id
+#     object_id = data.azurerm_client_config.current.object_id
+
+#     key_permissions = [
+#       "get",
+#     ]
+
+#     secret_permissions = [
+#       "get",
+#     ]
+
+#     storage_permissions = [
+#       "get",
+#     ]
+#   }
+
+  network_acls {
+    default_action = "Deny"
+    bypass         = "AzureServices"
+  }
+
+#   tags = {
+#     environment = "Testing"
+#   }
+}
+
+data "azurerm_key_vault" "example" {
+  name                = "my-key-vault"
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+output "vault_uri" {
+  value = data.azurerm_key_vault.example.vault_uri
+}
+
+resource "azurerm_key_vault_secret" "example" {
+  name         = "secret-sauce"
+  value        = "szechuan"
+  key_vault_id = azurerm_key_vault.example.id
+
+}
+
+# data "azurerm_key_vault_secret" "example" {
+#   name         = "secret-sauce"
+#   key_vault_id = data.azurerm_key_vault.existing.id
+# }
+
+# output "secret_value" {
+#   value = data.azurerm_key_vault_secret.example.value
+# }
